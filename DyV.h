49c8a70e0364ecfafbddfa29c349fe343c66d3cc
Jesús Mochales Domingo <jmocdom@etsinf.upv.es>
2025-10-02 11:14:41 +0200

    Modificaciones hechas.

--- DyV.h
+++ DyV.h
@@ -2,20 +2,50 @@
 #include <iostream>
 using namespace std;
 
-int BusquedaBinaria (int x, vector<int> a, int ini, int fin) {
-    if (ini > fin) { printf("No se ha encontrado el valor\n"); return -1; }
+
+template <typename T>
+int BusquedaBinaria(T x, const vector<T>& a, int ini, int fin) {
+    if (ini > fin) { 
+        cout << "No se ha encontrado el valor\n"; 
+        return -1; 
+    }
     int medio = (ini + fin) / 2;
     if (a[medio] == x) return medio;
     else if (a[medio] > x) return BusquedaBinaria(x, a, ini, medio - 1);
     else return BusquedaBinaria(x, a, medio + 1, fin);
-
 }
 
-int BusquedaBinariaINV (int x, vector<int> a, int ini, int fin) {
-    if (ini > fin) { printf("No se ha encontrado el valor\n"); return -1; }
+template <typename T>
+int BusquedaBinariaINV(T x, const vector<T>& a, int ini, int fin) {
+    if (ini > fin) { 
+        cout << "No se ha encontrado el valor\n"; 
+        return -1; 
+    }
     int medio = (ini + fin) / 2;
     if (a[medio] == x) return medio;
     else if (a[medio] < x) return BusquedaBinariaINV(x, a, ini, medio - 1);
     else return BusquedaBinariaINV(x, a, medio + 1, fin);
+}
 
+template <typename T>
+void QuickSort(vector<T>& a, int ini, int fin) {
+    if (ini < fin) {
+        int pivot = Partition(a, ini, fin);
+        QuickSort(a, ini, pivot - 1);
+        QuickSort(a, pivot + 1, fin);
+    }
+}
+
+template <typename T>
+int Partition(vector<T>& a, int ini, int fin) {
+    T x = a[fin];
+    int i = ini;
+    for (int j = ini; j < fin; j++) {
+        if (a[j] <= x) {
+            swap(a[i], a[j]);
+            i++;
+        } 
+    }
+    swap(a[i], a[fin]);
+    return i;
 }
--- test_DyV.cpp
+++ test_DyV.cpp
@@ -1,35 +1,7 @@
 #include <iostream>
 #include <vector>
+#include "DyV.h"
 using namespace std;
-
-// Si DyV.h no existe, agregamos la función plantilla aquí:
-template <typename T>
-int BusquedaBinaria(T x, const vector<T>& a, int inicio, int fin) {
-    while (inicio <= fin) {
-        int medio = inicio + (fin - inicio) / 2;
-        if (a[medio] == x)
-            return medio;
-        else if (a[medio] < x)
-            inicio = medio + 1;
-        else
-            fin = medio - 1;
-    }
-    return -1;
-}
-
-template <typename T>
-int BusquedaBinariaINV(T x, const vector<T>& a, int inicio, int fin) {
-    while (inicio <= fin) {
-        int medio = inicio + (fin - inicio) / 2;
-        if (a[medio] == x)
-            return medio;
-        else if (a[medio] > x)
-            inicio = medio + 1;
-        else
-            fin = medio - 1;
-    }
-    return -1;
-}
 
 int main() {
     vector<int> a = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
@@ -38,29 +10,39 @@ int main() {
     float y = 3.3f;
     vector<char> c = {'a', 'b', 'c', 'd', 'e'};
     char z = 'd';
+
     int resultado1 = BusquedaBinaria(x, a, 0, a.size() - 1);
     if (resultado1 != -1) {
         cout << "El valor " << x << " se encuentra en la posicion: " << resultado1 << endl;
     }
+
     int resultado2 = BusquedaBinaria(y, b, 0, b.size() - 1);
     if (resultado2 != -1) {
         cout << "El valor " << y << " se encuentra en la posicion: " << resultado2 << endl;
     }
+
     int resultado3 = BusquedaBinaria(z, c, 0, c.size() - 1);
     if (resultado3 != -1) {
         cout << "El valor " << z << " se encuentra en la posicion: " << resultado3 << endl;
     }
-    int resultado4 = BusquedaBinariaINV(5, {10, 9, 8, 7, 6, 5, 4, 3, 2, 1}, 0, 9);
+
+    vector<int> a_inv = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+    int resultado4 = BusquedaBinariaINV(5, a_inv, 0, a_inv.size() - 1);
     if (resultado4 != -1) {
         cout << "El valor 5 se encuentra en la posicion: " << resultado4 << endl;
     }
-    int resultado5 = BusquedaBinariaINV(3.3f, {5.5f, 4.4f, 3.3f, 2.2f, 1.1f}, 0, 4);
+
+    vector<float> b_inv = {5.5f, 4.4f, 3.3f, 2.2f, 1.1f};
+    int resultado5 = BusquedaBinariaINV(3.3f, b_inv, 0, b_inv.size() - 1);
     if (resultado5 != -1) {
         cout << "El valor 3.3 se encuentra en la posicion: " << resultado5 << endl;
     }
-    int resultado6 = BusquedaBinariaINV('d', {'e', 'd', 'c', 'b', 'a'}, 0, 4);
+
+    vector<char> c_inv = {'e', 'd', 'c', 'b', 'a'};
+    int resultado6 = BusquedaBinariaINV('d', c_inv, 0, c_inv.size() - 1);
     if (resultado6 != -1) {
         cout << "El valor 'd' se encuentra en la posicion: " << resultado6 << endl;
     }
+
     return 0;
 }
